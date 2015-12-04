4c8238268b5a78684e34411768d62ddae316729c
samuelricci <trumpetersamr@gmail.com>
2015-12-04 16:50:07 -0700

    Initial js commit

---
+++ intergalaticjustice/game.js
@@ -0,0 +1,13 @@
+var aiArray = ['rock', 'paper', 'scissors'];
+
+var aiChoice = aiArray[Math.floor(Math.random()*3)];
+
+$('#aiPick').html( aiChoice );
+
+// Listen for user click and then compare with aiChoice and display answer
+// depending on comparison logic
+
+$('pre').on('click', function(e) {
+  var playerChoose = e.target.id;
+  alert(playerChoose);
+});
