54a05f067f2acbcecd9f29d56240a4c76b89387d
samuelricci <trumpetersamr@gmail.com>
2015-12-04 17:01:03 -0700

    Update game.js

--- intergalaticjustice/game.js
+++ intergalaticjustice/game.js
@@ -7,7 +7,7 @@ $('#aiPick').html( aiChoice );
 // Listen for user click and then compare with aiChoice and display answer
 // depending on comparison logic
 
-// $('pre').on('click', function(e) {
-  // var playerChoose = e.target.id;
-  // alert(playerChoose);
-// });
+$('pre').on('click', function(e) {
+ var playerChoose = e.target.id;
+ alert(playerChoose);
+});
