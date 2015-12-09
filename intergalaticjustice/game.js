ac04108a1de34218087729c608d5515ae832e674
samuelricci <trumpetersamr@gmail.com>
2015-12-08 17:18:07 -0700

    Added logic

--- intergalaticjustice/game.js
+++ intergalaticjustice/game.js
@@ -2,12 +2,36 @@ var aiArray = ['rock', 'paper', 'scissors'];
 
 var aiChoice = aiArray[Math.floor(Math.random()*3)];
 
-$('#aiPick').html( aiChoice );
+$('#aiPick').html(aiChoice);
 
 // Listen for user click and then compare with aiChoice and display answer
 // depending on comparison logic
+var playerChoice = humanPick;
 
 $('pre').on('click', function(e) {
- var playerChoose = e.target.id;
- alert(playerChoose);
-});
+  var humanPick = e.target.id;
+  alert ( humanPick );
+}); 
+
+
+function compare (playerChoice, aiChoice) {
+  
+  var result = "";
+  
+  if (playerChoice === aiChoice) {
+    result = "tie";
+  }
+  
+  if (playerChoice === 'rock' && aiChoice === 'scissors' || playerChoice === 'paper' && aiChoice === 'rock' || playerChoice === 'scissors' && aiChoice === 'paper') {
+    result = "win";
+  }
+  
+  if (playerChoice === 'rock' && aiChoice === 'paper' || playerChoice === 'paper' && aiChoice === 'scissors' || playerChoice === 'scissors' && aiChoice === 'rock') {
+    result = "lose";
+  }
+  return (result);
+}
+
+
+
+$('#compareResult').html(endResult);
